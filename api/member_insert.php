6b359f16274dc8f2b703a528286b2f936b12dde2
lala11112 <lala11112@naver.com>
2022-05-12 20:07:30 +0900

    1.0

---
+++ api/member_insert.php
@@ -0,0 +1,21 @@
+<?php
+    $id   = $_POST["id"];
+    $pass = $_POST["pass"];
+
+              
+    $con = mysqli_connect("localhost", "user1", "12345", "pjwweb");
+
+	$sql = "INSERT INTO men(id, pass) ";
+	$sql .= "VALUES('$id', '$pass')";
+
+	mysqli_query($con, $sql);  // $sql 에 저장된 명령 실행
+    mysqli_close($con);     
+
+    echo "
+	      <script>
+	          location.href = '../index.php';
+	      </script>
+	  ";
+?>
+
+   
---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div>HEADER</div>
+
+        <li><a href="member_form.php">회원가입</a> </li>
+        <li><a href="login_form.php">로그인</a></li>
